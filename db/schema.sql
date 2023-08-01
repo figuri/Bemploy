22112401b7397aa41f889040b999ce4302e67470
unknown <samtisme@gmail.com>
2023-08-01 15:06:23 -0700

    added schema sql psuedo code

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- drop db if exists
+-- create db
+
+-- use db
+
+-- create department table
+-- create id and dept name columns
+
+-- create role table
+-- create id, title, salary, and department-id* columns
+
+-- create employee table
+-- create id, first name, last name, role-id*, and manager-id* columns
