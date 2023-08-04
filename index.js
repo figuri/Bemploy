54e61f06f7c2cef0f60456b971b5827111b6ef61
unknown <samtisme@gmail.com>
2023-08-03 22:26:52 -0700

    added updateEmployeeRole function [ trust me the functions do what they sound like i promise

--- index.js
+++ index.js
@@ -204,24 +204,161 @@ const addNewDepartment = () => {
 
 const addNewEmployee = () => {
     // ask name of employee
-    // ask role of employee
-    // ask manager of employee
-    // if yes, add manager to employee
-    // insert new employee into database
-    // return confirmation message
-    // call mainMenu()
+    inquirer.prompt([
+        // ask first name
+        {
+            type: 'input',
+            name: 'employeeFirstName',
+            message: 'What is the first name of the employee?',
+            validate: employeeFirstNameInput => {
+                if (employeeFirstNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a first name!');
+                    return false;
+                }
+            }
+        },
+        {
+            // ask last name
+            type: 'input',
+            name: 'employeeLastName',
+            message: 'What is the last name of the employee?',
+            validate: employeeLastNameInput => {
+                if (employeeLastNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a last name!');
+                    return false;
+                }
+            }
+        }
+        // get all roles from database
+    ]).then((answer) => {
+        const { employeeFirstName, employeeLastName } = answer;
+        db.promise().query('SELECT id, title FROM role')
+            .then(([roles]) => {
+                const roleChoices = roles.map((role) => ({
+                    name: role.title,
+                    value: role.id,
+                }));
+                inquirer.prompt([
+                    // ask role of employee
+                    {
+                        type: 'list',
+                        name: 'roleId',
+                        message: 'What is the role of the employee?',
+                        choices: roleChoices
+                    },
+                    // ask if employee has manager
+                ]).then((answer) => {
+                    const { roleId } = answer;
+                    inquirer.prompt([
+                        {
+                            type: 'confirm',
+                            name: 'hasManager',
+                            message: 'Does the employee have a manager?',
+                            default: false,
+                        },
+                    ]).then((answer) => {
+                        // if employee has manager, ask for manager
+                        const { hasManager } = answer;
+                        if (hasManager) {
+                            db.promise().query('SELECT id, first_name, last_name FROM employee')
+                                .then(([employees]) => {
+                                    const managerChoices = employees.map((employee) => ({
+                                        name: `${employee.first_name} ${employee.last_name}`,
+                                        value: employee.id,
+                                    }));
+                                    inquirer.prompt([
+                                        // ask for manager
+                                        {
+                                            type: 'list',
+                                            name: 'managerId',
+                                            message: 'Who is the manager of the employee?',
+                                            choices: managerChoices
+                                        },
+                                    ]).then((answer) => {
+                                        const { managerId } = answer;
+                                        // insert employee into database
+                                        db.promise().query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [employeeFirstName, employeeLastName, roleId, managerId])
+                                            .then(() => {
+                                                console.log('New employee added!');
+                                                mainMenu();
+                                            })
+                                            .catch((err) => {
+                                                console.error('Could not add employee', err);
+                                                mainMenu();
+                                            });
+                                    });
+                                });
+                                // if employee does not have manager, insert employee into database
+                        } else {
+                            db.promise().query('INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)', [employeeFirstName, employeeLastName, roleId])
+                                .then(() => {
+                                    console.log('New employee added!');
+                                    mainMenu();
+                                })
+                                .catch((err) => {
+                                    console.error('Could not add employee', err);
+                                    mainMenu();
+                                });
+                        }
+                    });
+                });
+            });
+    });
 }
 
 const updateEmployeeRole = () => {
-    //     // query database for list of employees
-    //     // give list of employees to choose from
-    //     // ask which employee to update
-    //     // query database for list of roles
-    //     // give list of roles to choose from
-    //     // ask which role to update to
-    //     // update employee role in database
-    //     // return confirmation message
-    //     // call mainMenu()
+    // ask which employee to update
+    db.promise().query('SELECT id, first_name, last_name FROM employee')
+        .then(([employees]) => {
+            const employeeChoices = employees.map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id,
+            }));
+            inquirer.prompt([
+                // ask which employee to update
+                {
+                    type: 'list',
+                    name: 'employeeId',
+                    message: 'Which employee would you like to update?',
+                    choices: employeeChoices
+                },
+            ]).then((answer) => {
+                const { employeeId } = answer;
+                // ask which role to update to
+                db.promise().query('SELECT id, title FROM role')
+                    .then(([roles]) => {
+                        const roleChoices = roles.map((role) => ({
+                            name: role.title,
+                            value: role.id,
+                        }));
+                        inquirer.prompt([
+                            // ask new role of employee
+                            {
+                                type: 'list',
+                                name: 'roleId',
+                                message: 'What is the new role of the employee?',
+                                choices: roleChoices
+                            },
+                        ]).then((answer) => {
+                            const { roleId } = answer;
+                            // update employee role in database
+                            db.promise().query('UPDATE employee SET role_id = ? WHERE id = ?', [roleId, employeeId])
+                                .then(() => {
+                                    console.log('Employee role updated!');
+                                    mainMenu();
+                                })
+                                .catch((err) => {
+                                    console.error('Could not update employee role', err);
+                                    mainMenu();
+                                });
+                        });
+                    });
+            });
+        });
 }
 
 mainMenu();
